fd5d211166e0180717d716016f652f6962d5a94c
Luisa Ghellere <54870171+ghellereluisa@users.noreply.github.com>
2021-06-24 22:55:05 -0300

    controller and some bug fixes

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import "reflect-metadata";
 import express, { request, response } from "express";
 import "./database";
+import { router } from "./routes";
 
 const app = express();
+
+app.use(express.json());
+
+app.use(router);
 
 app.listen(3000, () => console.log("server is running"));
