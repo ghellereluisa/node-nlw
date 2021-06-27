f747bfd9dbd7fcef42c2ac849e9739c68aa5b0da
Luisa Ghellere <54870171+ghellereluisa@users.noreply.github.com>
2021-06-27 15:46:58 -0300

    EnsureAdmin

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { CreateTagController } from "./controllers/CreateTagController";
+import { EnsureAdmin } from "./middlewares/EnsureAdmin";
 
 const router = Router();
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 router.post("/users", createUserController.handle);
-router.post("/tags", createTagController.handle);
+router.post("/tags", EnsureAdmin, createTagController.handle);
 
-export { router }
+export { router };
